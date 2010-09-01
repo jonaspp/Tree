e46d13ec69a08c584dd73a1e60ec0ac38c3e0693
jonaspp <jonaspp@c3f30751-cb38-88ff-ab2a-cef3b7904c16>
2010-09-01 00:06:59 +0000

    Created folders and namespaces, added Container, Injector, Lifecycle, TestCases

--- TreeTest/UnitTest.cs
+++ TreeTest/UnitTest.cs
@@ -3,24 +3,33 @@ using System.Text;
 using System.Collections.Generic;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Tree.Factory;
 using TreeTest.Impl;
+using Tree.Test;
+using Tree.Injector;
+using Tree.Factory;
+using Tree.Container;
 
 namespace TreeTest
 {
     [TestClass]
     public class UnitTest : TestCase
-    {        
-        public UnitTest()
-        {
-        }
+    {
+        [Inject()]
+        private ITest testObj;
 
         [TestMethod]
         public void TestFactory()
         {
-            ITest testObj = ObjectFactory.Register<ITest, TestImpl>();
+            Assert.IsTrue(ObjectContainer.Objects.ContainsValue(testObj));
+
             Assert.IsNotNull(testObj);
-            Assert.AreEqual("Job", testObj.Test());
+            Assert.AreEqual("Idle", testObj.Test());
+
+            ObjectContainer.Start();
+            Assert.AreEqual("Started", testObj.Test());
+
+            ObjectContainer.Stop();
+            Assert.AreEqual("Stopped", testObj.Test());
         }
     }
 }
